151c4f73ec4d775a3210e39b7471d260277208d1
Mashiur Rahman <anirbanmashiur@gmail.com>
2025-05-15 18:51:43 -0700

    Delete assets directory

--- assets/js/parallax.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', () => {
-    const scrollValue = window.scrollY;
-    const parallax = document.querySelector('.parallax-video');
-    parallax.style.transform = `translateY(${scrollValue * 0.4}px)`;
-  });
